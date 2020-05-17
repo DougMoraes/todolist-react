cb1ed18880256ce3a8543479ac7859320a90ff1b
Douglas <dougmoraes91@gmail.com>
2020-05-17 16:56:05 +0200

    feat: add api call to get todos from firebase

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,16 +1,25 @@
-exports.getAllTodos = (req, resp) => {
-  todos = [
-    {
-      id: "1",
-      title: "greeting",
-      body: "Hello world from Doug Moraes"
-    },
-    {
-      id: "2",
-      title: "greeting2",
-      body: "Hello2 world2 from Doug Moraes"
-    }
-  ];
+const { db } = require("../util/admin");
 
-  return resp.json(todos);
+exports.getAllTodos = (req, resp) => {
+  db.collection("todos")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let todos = [];
+      data.forEach(doc => {
+        todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: doc.data().createdAt
+        });
+      });
+
+      return resp.json(todos);
+    })
+    .catch(err => {
+      console.error(err);
+
+      return resp.status(500).json({ error: err.code });
+    });
 };
