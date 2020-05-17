fb5f107321a832dd9dcecf2571a885cdc4210f47
Douglas <dougmoraes91@gmail.com>
2020-05-17 18:40:37 +0200

    feat: create editTodo endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { getAllTodos, postOneTodo, deleteOneTodo } = require("./APIs/todos");
+const {
+  getAllTodos,
+  postOneTodo,
+  deleteOneTodo,
+  editTodo
+} = require("./APIs/todos");
 
 app.get("/todos", getAllTodos);
 app.post("/todo", postOneTodo);
 app.delete("/todo/:todoId", deleteOneTodo);
+app.put("/todo/:todoId", editTodo);
 
 exports.api = functions.https.onRequest(app);
