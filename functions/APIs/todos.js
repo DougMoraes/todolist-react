4eebc163a81c3610696cbd05f64a128db3ff1f5a
Douglas <dougmoraes91@gmail.com>
2020-05-17 17:55:23 +0200

    feat: create postOneTodo method

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -23,3 +23,31 @@ exports.getAllTodos = (req, resp) => {
       return resp.status(500).json({ error: err.code });
     });
 };
+
+exports.postOneTodo = (req, resp) => {
+  if (req.body.body.trim() === "") {
+    return resp.status(400).json({ body: "Must not be empty" });
+  }
+
+  if (req.body.title.trim() === "") {
+    return resp.status(400).json({ title: "Must not be empty" });
+  }
+
+  const newTodoItem = {
+    title: req.body.title,
+    body: req.body.body,
+    createdAt: new Date().toISOString()
+  };
+
+  db.collection("todos")
+    .add(newTodoItem)
+    .then(doc => {
+      const createdItem = newTodoItem;
+      createdItem.id = doc.id;
+      return resp.json(createdItem);
+    })
+    .catch(err => {
+      resp.status(500).json({ error: "Something went wrong" });
+      console.log(err);
+    });
+};
