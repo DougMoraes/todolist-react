12a15591c600b1f6e9e4e45b678b76d0f15c34e6
Douglas <dougmoraes91@gmail.com>
2020-05-19 00:16:47 +0200

    feaT: create config object for auth api

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBA4M0JYdLGyN6Qkl308Og_Ftyf9h51Bh8",
+  authDomain: "todo-list-react-72ba6.firebaseapp.com",
+  databaseURL: "https://todo-list-react-72ba6.firebaseio.com",
+  projectId: "todo-list-react-72ba6",
+  storageBucket: "todo-list-react-72ba6.appspot.com",
+  messagingSenderId: "602463496635",
+  appId: "1:602463496635:web:35e2aaa36b8b4975207c9e",
+  measurementId: "G-X837VN21R9"
+};
