7458ba86318c6eadb039bc15fdab905a35f6b31e
Douglas <dougmoraes91@gmail.com>
2020-05-19 20:48:16 +0200

    feat: create loginUser api call

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -1,5 +1,26 @@
-const {
-  loginUser
-} = require('./APIs/users')
+const firebase = require("firebase");
+const config = require("../util/config");
+const { validateLoginData } = require("../util/validators");
 
-app.post("/login", loginUser);
+firebase.initializeApp(config);
+
+exports.loginUser = (req, resp) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  const { valid, errors } = validateLoginData(user);
+
+  if (!valid) return resp.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => data.user.getIdToken())
+    .then(token => resp.json({ token }))
+    .catch(error => {
+      console.error(error);
+      return resp.status(403).json({ general: 'Wrong combination of user and password!' })
+    })
+};
