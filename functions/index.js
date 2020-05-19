741cc8c3844aed5e6efb9d12e014b711c9fa012d
Douglas <dougmoraes91@gmail.com>
2020-05-19 20:48:51 +0200

    feat: create api endpoint in the right place

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-
 const {
   getAllTodos,
   postOneTodo,
   deleteOneTodo,
   editTodo
 } = require("./APIs/todos");
+const { loginUser } = require("./APIs/users");
+
+app.post("/login", loginUser);
 
 app.get("/todos", getAllTodos);
 app.post("/todo", postOneTodo);
