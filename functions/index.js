3d328c41de687a9a24375620df189f8508d02205
Douglas <dougmoraes91@gmail.com>
2020-05-26 18:25:09 +0200

    feat: create endpoint for uploadProfilePhoto

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,17 @@
 const functions = require("firebase-functions");
 const app = require("express")();
+const auth = require("./util/auth");
 const {
   getAllTodos,
   postOneTodo,
   deleteOneTodo,
   editTodo
 } = require("./APIs/todos");
-const { loginUser, signUpUser } = require("./APIs/users");
+const { loginUser, signUpUser, uploadProfilePhoto } = require("./APIs/users");
 
 app.post("/login", loginUser);
 app.post("/signup", signUpUser);
+app.post("/user/image", auth, uploadProfilePhoto);
 
 app.get("/todos", getAllTodos);
 app.post("/todo", postOneTodo);
